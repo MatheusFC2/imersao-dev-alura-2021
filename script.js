54ba39293c892be8a2a89c863d9690e3537d67d1
Matheus Campos <matheusfreitascampos@gmail.com>
2021-03-24 10:28:34 -0300

    commit the js to do style projet

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-var dolar = prompt("Qual a cotação atual do dolar?")
-var preco = prompt("Qual o preço do produto em dolar?")
-parseFloat(dolar)
+/*var primeiroValor = parseInt(prompt("Qual a cotação atual do bitcoin?"))
+var segundoValor = parseInt(prompt("Qual a preço do produto em bitcoin?"))
+
+var resultado = primeiroValor * segundoValor
+
+document.write(resultado)*/
+
+
+
+/*
+var bitcoin = prompt("Qual a cotação atual do bitcoin?")
+var preco = prompt("Qual o preço do produto em bitcoin?")
+parseFloat(bitcoin)
 parseFloat(preco)
-preco = preco * dolar
+preco = preco * bitcoin
 preco = preco.toFixed(2)
-alert("O valor do produto em real é: " + preco)
+alert("O valor do produto em real é: " + preco) */
